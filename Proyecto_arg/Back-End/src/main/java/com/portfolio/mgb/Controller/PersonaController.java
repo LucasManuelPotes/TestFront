5584203963a765e1ed59e8505c49facbca433987
LucasManuelPotes <lucaspotesmartinez@gmail.com>
2023-01-03 18:40:08 -0300

    conectando el back

--- Proyecto_arg/Back-End/src/main/java/com/portfolio/mgb/Controller/PersonaController.java
+++ Proyecto_arg/Back-End/src/main/java/com/portfolio/mgb/Controller/PersonaController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost4200")
+@CrossOrigin(origins = "http://localhost:4200" )
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
@@ -53,6 +53,6 @@ public class PersonaController {
 }
      @GetMapping("/personas/traer/perfil")
      public Persona findPersona(){
-         return ipersonaService.findPersona((long)2);
+         return ipersonaService.findPersona((long)6);
      }
 }

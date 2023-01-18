36dac568b2c688fce4aed6e6df1bd3b11cca832d
LucasManuelPotes <lucaspotesmartinez@gmail.com>
2023-01-18 15:48:16 -0300

    back end

--- Proyecto_arg/Front-End/src/app/app.module.ts
+++ Proyecto_arg/Front-End/src/app/app.module.ts
@@ -14,6 +14,9 @@ import { HysComponent } from './components/hys/hys.component';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+
 
 
 
@@ -31,13 +34,16 @@ import { HttpClientModule } from '@angular/common/http';
     HysComponent,
     ProyectoComponent,
     FooterComponent,
+    HomeComponent,
+    LoginComponent,
     ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     NgCircleProgressModule.forRoot({}),
-    HttpClientModule
+    HttpClientModule,
+    
     
   ],
   providers: [],
--- Proyecto_arg/Front-End/src/app/components/logo-ap/logo-ap.component.ts
+++ Proyecto_arg/Front-End/src/app/components/logo-ap/logo-ap.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-logo-ap',
   templateUrl: './logo-ap.component.html',
   styleUrls: ['./logo-ap.component.css']
 })
-export class LogoAPComponent {
+export class LogoAPComponent implements OnInit {
+  ngOnInit(): void {
+
+  }
+  
 
 }

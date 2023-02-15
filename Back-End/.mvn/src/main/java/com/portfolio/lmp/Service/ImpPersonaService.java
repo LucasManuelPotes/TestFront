c2ced8ef5d51422568debdc7aefa29f28095a0fa
LucasManuelPotes <lucaspotesmartinez@gmail.com>
2023-02-15 18:27:16 -0300

    Actualizacion

---
+++ Back-End/.mvn/src/main/java/com/portfolio/lmp/Service/ImpPersonaService.java
@@ -0,0 +1,39 @@
+package com.portfolio.lmp.Service;
+
+import com.portfolio.lmp.Entity.Persona;
+import com.portfolio.lmp.Interface.IPersonaService;
+import com.portfolio.lmp.Repository.IPersonaRepository;
+import jakarta.transaction.Transactional;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class ImpPersonaService implements IPersonaService{
+    @Autowired IPersonaRepository ipersonaRepository;
+
+    @Override
+    public List<Persona> getPersona() {
+        List<Persona> persona = ipersonaRepository.findAll();
+        return persona;
+    }
+
+    @Override
+    public void savePersona(Persona persona) {
+       ipersonaRepository.save(persona);
+    }
+
+    @Override
+    public void deletePersona(Long id) {
+        ipersonaRepository.deleteById(id);
+    }
+
+    @Override
+    public Persona findPersona(Long id) {
+     Persona persona = ipersonaRepository.findById(id).orElse(null);
+     return persona;
+    }
+    
+}
+
---
+++ Back-End/.mvn/src/test/java/com/portfolio/lmp/LmpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.lmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

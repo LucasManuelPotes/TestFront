b75f3030116d5cf2c894513c9952d32be088ffeb
LucasManuelPotes <lucaspotesmartinez@gmail.com>
2023-03-16 19:10:39 -0300

    creacion de environment

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'frontendlmp1',
+    appId: '1:530407353348:web:19a3f633dbc5bcc491d951',
+    storageBucket: 'frontendlmp1.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyDTZUKvTUl-4TUrQ129zX38iCr-zEs3UqQ',
+    authDomain: 'frontendlmp1.firebaseapp.com',
+    messagingSenderId: '530407353348',
+  },
+    production: true,
+    URL: 'lucas-developer-lucasmanuelpotesk5gm26ifcq.koyeb.app/'
+  };
---
+++ src/environments/environment.ts
@@ -0,0 +1,28 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'frontendlmp1',
+    appId: '1:530407353348:web:19a3f633dbc5bcc491d951',
+    storageBucket: 'frontendlmp1.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyDTZUKvTUl-4TUrQ129zX38iCr-zEs3UqQ',
+    authDomain: 'frontendlmp1.firebaseapp.com',
+    messagingSenderId: '530407353348',
+  },
+  
+      production: false,
+      URL: 'http://localhost:8080/'
+      //URL: 'https://smydeveloper-williams1355.koyeb.app/'
+    };
+    
+    /*
+     * For easier debugging in development mode, you can import the following file
+     * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+     *
+     * This import should be commented out in production mode because it will have a negative impact
+     * on performance if an error is thrown.
+     */
+    // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.

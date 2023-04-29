d1283cc5b1fae756cef9b7fe5ef35872edbc4cb8
LucasManuelPotes <lucaspotesmartinez@gmail.com>
2023-04-29 12:51:31 -0300

    correcion

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   firebase: {
     projectId: 'frontendlmp1',
-    appId: '1:530407353348:web:19a3f633dbc5bcc491d951',
+    appId: '1:530407353348:web:5db41ed760380ccb91d951',
     storageBucket: 'frontendlmp1.appspot.com',
     locationId: 'southamerica-east1',
     apiKey: 'AIzaSyDTZUKvTUl-4TUrQ129zX38iCr-zEs3UqQ',
